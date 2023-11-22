70c14e68a94cb713d5e4f2dad9dead5952185f42
yhonygbc <yhonygbc@gmail.com>
2023-11-22 14:27:10 -0500

    primer commit

---
+++ app/src/main/java/com/example/retrofit/data/RetrofitService.kt
@@ -0,0 +1,23 @@
+package com.example.retrofit.data
+
+import com.example.retrofit.data.model.RemoteResult
+import retrofit2.Retrofit
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RetrofitService {
+
+
+
+    @GET("discover/movie?sort_by=popularity.desc")
+    suspend fun listPopularMovies(
+        @Query("api_key") apiKey:String,
+        @Query("language") language: String
+    ): RemoteResult
+
+    object RetrofitServiceFactory {
+        fun makeRetrofitService(): RetrofitService {
+            return  Retrofit.Builder().baseUrl("https://api.themoviedb.org/3/").build().create(RetrofitService::class.java)
+        }
+    }
+}
